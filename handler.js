5c4725aaa29de8744f882010ec9b7b7a0c87b9c1
Phodal HUANG <h@phodal.com>
2018-02-26 21:47:38 +0800

    [T] create basic services

--- handler.js
+++ handler.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
+module.exports.get = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
@@ -10,7 +10,17 @@ module.exports.hello = (event, context, callback) => {
   };
 
   callback(null, response);
+};
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+
+module.exports.create = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  };
+
+  callback(null, response);
 };
